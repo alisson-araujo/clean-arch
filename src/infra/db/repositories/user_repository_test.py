ee2ae59f7649d5dd9626443b68968761d12d274e
Alisson Araujo <alisson.ads@hotmail.com>
2023-07-18 17:54:36 -0300

    user registration test

---
+++ src/infra/db/repositories/user_repository_test.py
@@ -0,0 +1,35 @@
+import pytest
+from sqlalchemy import text
+from src.infra.db.settings.connection import DBConnectionHandler
+from .user_repository import UserRepository
+
+db_connection_handler = DBConnectionHandler()
+connection = db_connection_handler.get_engine().connect()
+
+@pytest.mark.skip(reason="Sensitive test")
+def test_insert_user():
+    mocked_first_name = "Homer"
+    mocked_last_name = "Simpson"
+    mocked_age = 34
+
+    user_repository = UserRepository()
+    user_repository.insert_user(mocked_first_name, mocked_last_name, mocked_age)
+
+    query = """
+        SELECT * FROM users 
+        WHERE first_name='{}'
+        AND last_name='{}'
+        AND age='{}';""".format(
+        mocked_first_name, mocked_last_name, mocked_age,
+    )
+    response = connection.execute(text(query))
+    registry = response.fetchall()[0]
+
+    print(registry)
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+    connection.execute(text(f"""DELETE FROM users WHERE id = {registry.id}"""))
+    connection.commit()
