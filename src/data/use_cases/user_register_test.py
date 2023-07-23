41c0e267e3f0658f0fb87660be3bf89bae398054
Alisson Araujo <alisson.ads@hotmail.com>
2023-07-23 14:23:03 -0300

    implementing user register

---
+++ src/data/use_cases/user_register_test.py
@@ -0,0 +1,17 @@
+from src.infra.db.tests.users_repository import UserRepositorySpy
+from .user_register import UserRegister
+
+def test_register():
+    first_name = "Lucas"
+    last_name = "Santos"
+    age = 25
+
+    user_repository = UserRepositorySpy()
+    user_register = UserRegister(user_repository)
+
+    response = user_register.register(first_name, last_name, age)
+    print(user_repository.insert_user_attributes)
+
+    assert response["type"] == "Users"
+    assert response["count"] == 1
+    assert response["attributes"]
