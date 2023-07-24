619c4f1a095296376cf12f0326e68a84d8d667db
Alisson Araujo <alisson.ads@hotmail.com>
2023-07-23 23:41:03 -0300

    adding flask and routes

--- src/main/composers/user_finder_composer.py
+++ src/main/composers/user_finder_composer.py
@@ -3,7 +3,7 @@ from src.data.use_cases.user_finder import UserFinder
 from src.presentation.controllers.user_finder_controller import UserFinderController
 
 
-def user_register_composer():
+def user_finder_composer():
     repository = UsersRepository()
     use_case = UserFinder(repository)
     controller = UserFinderController(use_case)
